0af0fbf3e35a0ba4db2892756e2ccb5a8ee1e1fe
unknown <lucasjavierf10@gmail.com>
2025-07-28 16:41:39 -0300

    Termine por el momento el footer

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Hero from '../../componentes/Hero'
 
 
 export const Home = () => {
   return (
     <>
+      <Hero/>
       <h2 className='text-3xl font-bold underline text-blue-500 bg-gray-200 h-dvh text-center justify-center pt-6'>contenido de Home</h2>
       
     </>
