3a6af6f302f6935c029b4ff73a178e323c9b2f85
unknown <lucasjavierf10@gmail.com>
2025-07-30 18:00:19 -0300

    cambio en nosotros.jsx

--- src/pages/client/Nosotros.jsx
+++ src/pages/client/Nosotros.jsx
@@ -8,12 +8,4 @@ export const Nosotros = () => {
 
 export default Nosotros;
 
-import React from 'react'
 
-export const Nosotros = () => {
-  return (
-    <h2 className=' text-3xl font-bold underline text-blue-500 bg-gray-200 h-dvh text-center justify-center pt-6'>contenido de Nosotros</h2>
-  )
-}
-
-export default Nosotros;
